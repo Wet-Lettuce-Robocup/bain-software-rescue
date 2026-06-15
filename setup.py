767af3735b3b74633f5be9906cf769bc1e098cb3
TheRustedBeasty <bchangson@proton.me>
2026-06-16 09:43:54 +1000

    corrected setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'rescue_state_bc'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     package_data={'': ['py.typed']},
     install_requires=['setuptools'],
@@ -25,6 +29,9 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'rescue_node = rescue_state_bc.rescue_node:main',
+            'front_vision_node = rescue_state_bc.front_vision_node:main',
+            'down_vision_node = rescue_state_bc.down_vision_node:main',
         ],
     },
 )
