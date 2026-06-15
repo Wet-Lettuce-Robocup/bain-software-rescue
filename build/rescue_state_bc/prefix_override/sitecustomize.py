bc152356cfe4556e91595a00921b8aebf044cee2
TheRustedBeasty <bchangson@proton.me>
2026-06-15 15:10:58 +1000

    remove cache

--- build/rescue_state_bc/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/bain/Documents/GitHub/robocup-ros/ros/bain-software-rescue/install/rescue_state_bc'
