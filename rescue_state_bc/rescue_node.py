fb21f38123c0c14ac6efdd2d52920f496ff60464
TheRustedBeasty <bchangson@proton.me>
2026-06-26 10:06:35 +1000

    change m_to distance mult

--- rescue_state_bc/rescue_node.py
+++ rescue_state_bc/rescue_node.py
@@ -150,7 +150,7 @@ class Rescue(LifecycleNode):
     black_line_seen = False
     search_step = 0
     rad_to_turn = 2
-    m_to_dist = 1000
+    m_to_dist = 700
 
     def __init__(self):
         super().__init__('rescue_node')
