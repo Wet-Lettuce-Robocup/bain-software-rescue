70fd778a37d2a06bc882bc658881748a5c0057a4
TheRustedBeasty <bchangson@proton.me>
2026-06-26 10:14:44 +1000

    increaase rad to turn multiplier

--- rescue_state_bc/rescue_node.py
+++ rescue_state_bc/rescue_node.py
@@ -149,7 +149,7 @@ class Rescue(LifecycleNode):
     exit_kp = 0.5
     black_line_seen = False
     search_step = 0
-    rad_to_turn = 2
+    rad_to_turn = 200
     m_to_dist = 700
 
     def __init__(self):
