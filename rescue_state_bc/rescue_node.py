b4881febf22e511e55f76645319127ac26e8d579
TheRustedBeasty <bchangson@proton.me>
2026-06-27 13:28:52 +1000

    decrease vel again

--- rescue_state_bc/rescue_node.py
+++ rescue_state_bc/rescue_node.py
@@ -460,7 +460,7 @@ class Rescue(LifecycleNode):
             self.grab('open')
             self.get_logger().info(f'Lowered lift and opened claw for victim "{self.target_type}"')
             if not getattr(self.move, 'busy', False):
-                self.publish_cmd_vel(0.7, 0.2)
+                self.publish_cmd_vel(0.07, 0.02)
                 self.state = 10
 
         # when claw is close enough stop
